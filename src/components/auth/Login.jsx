053dd4548f9f806728e7a1144f6e5cb358d48265
KeniKT <keni232127@gmail.com>
2023-10-23 07:47:04 +0300

    integrated the sign-in with the backend to successfully sign-in

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,40 +1,74 @@
-import React from 'react'
-import "./auth.css"
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import "./auth.css";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleRoute = () => {
-    fetch("localhost:3000/login", {
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const loginData = {
+      client:{
+        email: email,
+        password: password
+      }
+    };
+
+    fetch("http://127.0.0.1:3000/clients/sign_in", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: {
-        "username": "user",
-        "password": "pass"
-      }
+      body: JSON.stringify(loginData)
     })
-    navigate("/register")
+      .then(async response => {
+        if (response.ok) {
+          navigate("/home");
+        } else {
+          console.error("Login failed");
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
+
   return (
     <>
-
-        <header>
-            <img className="logo" src="/images/logos/logo.png" alt="Logo" />
-        </header>
-        <div className = "main-body">
-            <form action="">
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input className = "button" type="submit" value="Sign-in" />
-                <p style={{ color: "white", cursor: "pointer", display: "flex"}}>Dont have an Account? <p style={{ marginLeft: "10px", color: "#E1AF64", cursor: "pointer"}} onClick={handleRoute}>Sign Up</p></p>
-            </form>
-        </div>
-      
+      <header>
+        <img className="logo" src="/images/logos/logo.png" alt="Logo" />
+      </header>
+      <div className="main-body">
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input className="button" type="submit" value="Sign-in" />
+          <p style={{ color: "white", cursor: "pointer", display: "flex" }}>
+            Don't have an Account?{' '}
+            <span
+              style={{ marginLeft: "10px", color: "#E1AF64", cursor: "pointer" }}
+              onClick={() => navigate("/register")}
+            >
+              Sign Up
+            </span>
+          </p>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
