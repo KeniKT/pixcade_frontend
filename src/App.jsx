7c50a070228f1d33a9e4bdb9b0d2d11040d26da7
KeniKT <keni232127@gmail.com>
2023-10-23 07:47:29 +0300

    added the route to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import HomePage from "./components/home/HomePage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<HomePage />} />
 
       </Routes>
     </Router>
