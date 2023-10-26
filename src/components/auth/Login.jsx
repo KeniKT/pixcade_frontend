5eef5b37ef7e7465e7d036e55a42c0eeed62992d
alazarlemma02 <alazar03lemma@gmail.com>
2023-10-26 09:45:37 +0300

    made some changes to auth

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,40 +1,43 @@
-import React, { useState } from 'react';
-// import "./auth.css";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./auth.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-
-    const loginData = {
-      client:{
-        email: email,
-        password: password
-      }
-    };
-
-    fetch("http://127.0.0.1:3000/clients/sign_in", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(loginData)
-    })
-      .then(async response => {
-        if (response.ok) {
-          navigate("/home");
-        } else {
-          console.error("Login failed");
-        }
-      })
-      .catch(error => {
-        console.error(error);
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await fetch("http://127.0.0.1:3000/sign_in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            email,
+            password,
+          },
+        }),
       });
-  }
+
+      if (response.ok) {
+        const data = await response.json();
+        if (data["success"]) {
+          localStorage.setItem("token", data["jwt"]);
+          localStorage.setItem("username", data["user"]["full_name"]);
+          localStorage.setItem("email", data["user"]["email"]);
+          localStorage.setItem("user_id", data["user"]["id"]);
+          navigate("/home");
+        }
+      } else {
+        console.error("Authentication failed");
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+    }
+  };
 
   return (
     <>
@@ -55,11 +58,22 @@ const Login = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <input className="button" type="submit" value="Sign-in" />
+          <input
+            className="button"
+            type="submit"
+            value="Sign-in"
+            onClickCapture={() => {
+              handleLogin(email, password);
+            }}
+          />
           <p style={{ color: "white", cursor: "pointer", display: "flex" }}>
-            Don't have an Account?{' '}
+            Don't have an Account?{" "}
             <span
-              style={{ marginLeft: "10px", color: "#E1AF64", cursor: "pointer" }}
+              style={{
+                marginLeft: "10px",
+                color: "#E1AF64",
+                cursor: "pointer",
+              }}
               onClick={() => navigate("/register")}
             >
               Sign Up
@@ -69,6 +83,6 @@ const Login = () => {
       </div>
     </>
   );
-}
+};
 
 export default Login;
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,34 +1,42 @@
 import React, { useState } from 'react';
-// import "./auth.css";
+import "./auth.css";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const navigate = useNavigate();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [displayName, setDisplayName] = useState("");
+  const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [userType, setUserType] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+
 
   const handleRegistration = (e) => {
     e.preventDefault();
-  
+
     if (password !== confirmPassword) {
       setPasswordError("Passwords do not match");
       return;
     }
-  
-    fetch("http://localhost:3000/clients", {
+
+    fetch("http://localhost:3000/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        client: {
+        payload: {
+          first_name: firstName,
+          last_name: lastName,
+          user_name: userName,
           email: email,
-          password: password
+          password: password,
+          user_type: userType,
+          date_of_birth: dateOfBirth
         }
       })
     })
@@ -44,8 +52,8 @@ const Register = () => {
         console.error(error);
       });
   }
-  
-  
+
+
 
   return (
     <>
@@ -57,16 +65,17 @@ const Register = () => {
           <h2 className="header-title">Welcome!</h2>
           <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
           <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-          <input type="text" placeholder="Display Name" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
+          <input type="text" placeholder="Username" value={userName} onChange={(e) => setUserName(e.target.value)} />
           <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <input type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
           {passwordError && <p style={{ color: "red" }}>{passwordError}</p>}
-          <input type="date" placeholder="Date" />
-          <select id="account-type-select" placeholder="Email">
-            <option value="1">Developer</option>
-            <option value="2">Gamer</option>
+          <input type="date" placeholder="Date of Birth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
+          <select id="account-type-select" value={userType} onChange={(e) => setUserType(e.target.value)}>
+            <option value="developer">Developer</option>
+            <option value="gamer">Gamer</option>
           </select>
+
           <input className="button" type="submit" value="Sign-up" />
           <p style={{ color: "white", cursor: "pointer", display: "flex" }}>Already have an Account? <p style={{ marginLeft: "10px", color: "#E1AF64", cursor: "pointer" }} onClick={() => navigate("/")}>Login</p></p>
         </form>
